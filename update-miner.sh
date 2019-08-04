b42ecf659bf831f04f99ad043eaaf29c7ac348af
Jason Xie <jason.xie@victheme.com>
2019-08-04 20:39:56 +0800

    cleanup

--- update-miner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Updating Miner"
-
-ssh -t duckz@192.168.1.191 'sudo dpkg -i /home/duckz/packages/python-jxminer_0.3.21_all.deb'
-ssh -t duckz@192.168.1.192 'sudo dpkg -i /home/duckz/packages/python-jxminer_0.3.21_all.deb'
-ssh -t duckz@192.168.1.193 'sudo dpkg -i /home/duckz/packages/python-jxminer_0.3.21_all.deb'
-ssh -t duckz@192.168.1.194 'sudo dpkg -i /home/duckz/packages/python-jxminer_0.3.21_all.deb'
-ssh -t duckz@192.168.1.195 'sudo dpkg -i /home/duckz/packages/python-jxminer_0.3.21_all.deb'
